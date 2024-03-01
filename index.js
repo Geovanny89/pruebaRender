d877a519589011181ca0378bbcce41d0804c7e79
Geovanny89 <gcasadiegosr13@gmail.com>
2024-03-01 10:33:00 -0500

    se soluciona error

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ import pg from 'pg'
 config()
 
 const app = express();
-const pool = new pg.Pool({connectionString: process.env.DATABASE_URL,
-ssl:true});
+const pool = new pg.Pool({connectionString: process.env.DATABASE_URL
+// ssl:true
+});
 app.get('/ping', async (req, res) => {
     const result = await pool.query('SELECT NOW()')
     return res.json(result.rows[0])
